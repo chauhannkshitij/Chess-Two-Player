862418b3186e03b593f51a497d085257d48dd8b3
chauhannkshitij <kshitijchauhan7499@gmail.com>
2025-08-07 12:48:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess } = require("chess.js");
+const path = require("path");
+const { title } = require("process");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayers = "W";
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Welcome to Chess Game" });
+});
+
+io.on("connection", function (uniqueSocket) {
+  console.log("Socket io is  connected");
+  if (!players.white) {
+    players.white = uniqueSocket.id;
+    uniqueSocket.emit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = uniqueSocket.id;
+    uniqueSocket.emit("playerRole", "b");
+  } else {
+    uniqueSocket.emit("spectatorRole");
+  }
+
+  uniqueSocket.on("disconnect", function () {
+    if (uniqueSocket.id === players.white) {
+      delete players.white;
+    } else if (uniqueSocket.id === players.black) {
+      delete players.black;
+    }
+  });
+
+  uniqueSocket.on("move", (move) => {
+    try {
+      if (chess.turn() === "w" && uniqueSocket.id !== players.white) return;
+      if (chess.turn() === "b" && uniqueSocket.id !== players.black) return;
+
+      const result = chess.move(move);
+      if (result) {
+        currentPlayers = chess.turn();
+        io.emit("move", move);
+        io.emit("boardState", chess.fen());
+      } else {
+        console.log("Invalid move: ", move);
+        uniqueSocket.emit("Invalid move", move);
+      }
+    } catch (err) {
+      console.log(err);
+      uniqueSocket.emit("Invalid move", move);
+    }
+  });
+});
+
+server.listen(3000, function () {
+  console.log("Server started on PORT 3000");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './views/**/*.ejs',  
+    './public/**/*.{html,js}',
+    './public/js/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
